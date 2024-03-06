a3865b1efb29a23c096b7b5ba2843ae6dfe04272
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-03-06 20:27:16 -0300

    feat: generates outcome form component

---
+++ src/app/components/outcome-form/outcome-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outcome-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './outcome-form.component.html',
+})
+export class OutcomeFormComponent {}
