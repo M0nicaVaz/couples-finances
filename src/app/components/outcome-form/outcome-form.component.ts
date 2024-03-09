5d3902331171da47258eebf3831468d620b1d0ef
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-03-09 11:46:39 -0300

    refactor: adding button component to outcomeform

--- src/app/components/outcome-form/outcome-form.component.ts
+++ src/app/components/outcome-form/outcome-form.component.ts
@@ -1,19 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { FinancesService } from '../../services/finances.service';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { ionTrendingDown } from '@ng-icons/ionicons';
+import { ionTrendingDown, ionClose } from '@ng-icons/ionicons';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-outcome-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIconComponent],
-  viewProviders: [provideIcons({ ionTrendingDown })],
+  imports: [ReactiveFormsModule, NgIconComponent, ButtonComponent],
+  viewProviders: [provideIcons({ ionTrendingDown, ionClose })],
   templateUrl: './outcome-form.component.html',
   providers: [FinancesService],
 })
@@ -28,21 +29,27 @@ export class OutcomeFormComponent {
     });
   }
 
+  onCancel() {
+    this.outcomeForm.reset();
+  }
+
   onSubmit() {
     this.loading.set(true);
-    console.log(this.outcomeForm.value);
 
-    // if (this.outcomeForm.valid) {
-    //   this.financesService
-    //     .addOutcome('outcome', this.outcomeForm.value)
-    //     .subscribe({
-    //       next: (response) => {
-    //         console.log(response);
-    //         this.outcomeForm.reset();
-    //         this.loading.set(false);
-    //       },
-    //       error: (error) => console.log(error),
-    //     });
-    // }
+    if (this.outcomeForm.valid) {
+      this.financesService
+        .addOutcome('outcome', this.outcomeForm.value)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            this.outcomeForm.reset();
+            this.loading.set(false);
+          },
+          error: (error) => {
+            this.loading.set(false);
+            console.log(error);
+          },
+        });
+    }
   }
 }
