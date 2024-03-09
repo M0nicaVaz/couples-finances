dcf7e9b35a070dfa8d8200a17742613a3cab4e38
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-03-09 17:47:40 -0300

    refactor: avoiding api calls

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,24 +3,25 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionAddOutline } from '@ng-icons/ionicons';
 import { FinancesService } from '../../services/finances.service';
 import { OutcomeFormComponent } from '../../components/outcome-form/outcome-form.component';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgIconComponent, OutcomeFormComponent],
+  imports: [NgIconComponent, OutcomeFormComponent, ModalComponent],
   viewProviders: [provideIcons({ ionAddOutline })],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
   constructor(private financesService: FinancesService) {}
 
-  ngOnInit(): void {
-    this.financesService.getOutcomes('/outcomes').subscribe((outcomes) => {
-      console.log(outcomes);
-    });
+  // ngOnInit(): void {
+  //   this.financesService.getOutcomes('outcomes').subscribe((outcomes) => {
+  //     console.log(outcomes);
+  //   });
 
-    this.financesService.getIncomes('/incomes').subscribe((outcomes) => {
-      console.log(outcomes);
-    });
-  }
+  //   this.financesService.getIncomes('incomes').subscribe((outcomes) => {
+  //     console.log(outcomes);
+  //   });
+  // }
 }
