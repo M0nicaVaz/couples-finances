2a8c5d87f698e1853a80dfc731b0cbaad4e6492e
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-03-11 18:19:50 -0300

    feat: modal variants

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,12 +1,14 @@
 import { Component, ViewChild, ElementRef, Input } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { ionClose, ionAdd } from '@ng-icons/ionicons';
+import { ionClose } from '@ng-icons/ionicons';
+import { ButtonComponent } from '../button/button.component';
+import { ButtonVariants } from '../../interfaces/button.interface';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [NgIconComponent],
-  viewProviders: [provideIcons({ ionClose, ionAdd })],
+  imports: [NgIconComponent, ButtonComponent],
+  viewProviders: [provideIcons({ ionClose })],
   templateUrl: './modal.component.html',
 })
 export class ModalComponent {
@@ -14,6 +16,7 @@ export class ModalComponent {
 
   @ViewChild('dialog') dialog: ElementRef = {} as ElementRef;
   @Input({ required: true }) triggerText: string = '';
+  @Input() triggerVariant: ButtonVariants = 'cancel';
 
   openDialog() {
     this.dialog.nativeElement.showModal();
