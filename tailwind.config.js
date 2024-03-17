a6558df7e508eba1dcf82b9bdefad440675ed1f5
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-03-16 23:30:53 -0300

    feat: add dove color and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dove: {
+          50: "#f6f6f6",
+          100: "#e7e7e7",
+          200: "#d1d1d1",
+          300: "#b0b0b0",
+          400: "#888888",
+          500: "#6b6b6b",
+          600: "#5d5d5d",
+          700: "#4f4f4f",
+          800: "#454545",
+          900: "#3d3d3d",
+          950: "#262626",
+        },
+      },
+    },
   },
   plugins: [],
 };
